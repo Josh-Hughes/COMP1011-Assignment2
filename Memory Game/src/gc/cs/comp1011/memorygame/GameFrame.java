8626ae06daf19b9b3b0488bbfdfdcd9e69e374d3
Josh <Josh-Hughes@users.noreply.github.com>
2014-11-12 13:35:37 -0500

    Center align labels

--- Memory Game/src/gc/cs/comp1011/memorygame/GameFrame.java
+++ Memory Game/src/gc/cs/comp1011/memorygame/GameFrame.java
@@ -24,12 +24,12 @@ public class GameFrame extends JFrame {
 	public GameFrame() {
 		super("Memory Game");
 		setLayout(new BorderLayout());
-		
+
 		// Create the layout panels.
 		labelPanel = new JPanel(new GridLayout(2, 3));
 		cardsPanel = new JPanel(new GridLayout(4, 4));
 		buttonPanel = new JPanel(new GridLayout(1, 3));
-		
+
 		// Add panels to window.
 		add(labelPanel, BorderLayout.NORTH);
 		add(cardsPanel, BorderLayout.CENTER);
@@ -38,27 +38,29 @@ public class GameFrame extends JFrame {
 		// Create the buttons.
 		playButton = new JButton("Play Game");
 		exitButton = new JButton("Exit");
+
+		// Add buttons to button panel.
 		buttonPanel.add(playButton);
 		buttonPanel.add(exitButton);
 
 		// Create the labels.
-		highScoreText = new JLabel("High Score");
-		timerText = new JLabel("Time");
-		scoreText = new JLabel("Score");
-		highScore = new JLabel("####");
-		timer = new JLabel("##");
-		score = new JLabel("####");
+		highScoreText = new JLabel("High Score", SwingConstants.CENTER);
+		timerText = new JLabel("Time", SwingConstants.CENTER);
+		scoreText = new JLabel("Score", SwingConstants.CENTER);
+		highScore = new JLabel("####", SwingConstants.CENTER);
+		timer = new JLabel("##", SwingConstants.CENTER);
+		score = new JLabel("####", SwingConstants.CENTER);
+
+		// Set properties and stuff.
+		highScore.setSize(100, 20);
+
+		// Add labels to label panel.
 		labelPanel.add(highScoreText);
 		labelPanel.add(timerText);
 		labelPanel.add(scoreText);
 		labelPanel.add(highScore);
 		labelPanel.add(timer);
 		labelPanel.add(score);
-		
-		//plainButton.setText("Test Button");
-		//highScore.setText("Test");
-		
-		highScore.setSize(100, 20);
 
-	}// of constructor
+	}
 }
