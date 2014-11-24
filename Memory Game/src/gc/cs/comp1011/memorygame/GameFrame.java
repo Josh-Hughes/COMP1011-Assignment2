660bc6730a955bebd6968ad917a7e9d678dcdb16
luisxcam <luisxcam@gmail.com>
2014-11-24 17:02:27 -0500

    Created timer class.

--- Memory Game/src/gc/cs/comp1011/memorygame/GameFrame.java
+++ Memory Game/src/gc/cs/comp1011/memorygame/GameFrame.java
@@ -13,10 +13,6 @@ package gc.cs.comp1011.memorygame;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
