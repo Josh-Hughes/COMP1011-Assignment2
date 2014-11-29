f9f278eb82a09c41c98d5db3d69ef966e970e298
luisxcam <luisxcam@gmail.com>
2014-11-28 23:06:09 -0500

    GameMessage commented

--- Memory Game/src/gc/cs/comp1011/memorygame/GameMessages.java
+++ Memory Game/src/gc/cs/comp1011/memorygame/GameMessages.java
@@ -1,5 +1,10 @@
 package gc.cs.comp1011.memorygame;
-
+/**
+ * @filename: GameMessages.java
+ * @author Josh Hughes & Luis Acevedo
+ * @version November 28th, 2014
+ * @description Enum type to hold the messages from the system
+ */
 public enum GameMessages {
 	// Enumerations
 	FIRST_CARD("Pick a card, any card."),
@@ -13,14 +18,26 @@ public enum GameMessages {
 	private final String message;
 	
 	// Enum methods
+	/**
+	 * Sets the message to the enum value required
+	 * @param message required by user
+	 */
 	GameMessages(String message) {
 		this.message = message;
 	}
 	
+	/**
+	 * Returns the current status of the enum type
+	 * @return the value of the message required by the user
+	 */
 	public String getMessage() {
 		return message;
 	}
 	
+	/**
+	 * Returns a string with a message
+	 * @return the message required 
+	 */
 	@Override
 	public String toString() {
 		return getMessage();
